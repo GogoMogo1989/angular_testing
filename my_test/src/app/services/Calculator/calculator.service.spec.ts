c67a677bb57ae960b477693813ea443f9cf05a3c
GogoMogo1989 <kovacsfarkasdavid@gmail.com>
2023-08-14 18:02:13 +0200

    bcsdj

--- my_test/src/app/services/Calculator/calculator.service.spec.ts
+++ my_test/src/app/services/Calculator/calculator.service.spec.ts
@@ -4,14 +4,18 @@ import { CalculatorService } from './calculator.service';
 describe('CalculatorService', () => {
   it('should add two numbers', () => {
     let loggerService = new LoggerService();
+    spyOn(loggerService, 'log');
     const calculator = new CalculatorService(loggerService);
     let result = calculator.add(2, 2);
     expect(result).toBe(4);
+    expect(loggerService.log).toHaveBeenCalledTimes(1);
   });
   it('should subtrack two numbers', () => {
     let loggerService = new LoggerService();
+    spyOn(loggerService, 'log');
     const calculator = new CalculatorService(loggerService);
     let result = calculator.subtrack(2, 2);
     expect(result).toBe(0);
+    expect(loggerService.log).toHaveBeenCalledTimes(1);
   });
 });
--- my_test/src/app/services/Logger/logger.service.ts
+++ my_test/src/app/services/Logger/logger.service.ts
@@ -6,6 +6,7 @@ import { Injectable } from '@angular/core';
 export class LoggerService {
   messages: string[] = [];
   log(message: string) {
+    debugger;
     this.messages.push(message);
   }
 }

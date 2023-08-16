811cb94d33ab2b6ba816aa8e7515895077854298
GogoMogo1989 <kovacsfarkasdavid@gmail.com>
2023-08-16 15:02:39 +0200

    2023.08.16  33

---
+++ my_test/src/app/services/Post/httpClient.testing.spec.ts
@@ -0,0 +1,32 @@
+import { HttpClient } from '@angular/common/http';
+import { TestBed } from '@angular/core/testing';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
+
+let testUrl = '/data';
+interface Data {
+  name: string;
+}
+describe('http client testing moduel', () => {
+  let httpClient: HttpClient;
+  let httpTestingController: HttpTestingController;
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    });
+    httpClient = TestBed.inject(HttpClient);
+    httpTestingController = TestBed.inject(HttpTestingController);
+  });
+
+  it('should call the testurl with get Requies', () => {
+    const testData: Data = { name: 'Kovács' };
+    httpClient.get<Data>(testUrl).subscribe((data) => {
+      expect(data).toEqual(testData);
+    });
+    const request = httpTestingController.expectOne('/data');
+    request.flush(testData);
+    expect(request.request.method).toBe('GET');
+  });
+});
